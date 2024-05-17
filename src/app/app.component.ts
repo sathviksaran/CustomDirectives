a920f6be45d12fee45f4a8b6ff8d10768d4fe704
Sathvik Saran Atchukolu <94753192+sathviksaran@users.noreply.github.com>
2024-05-17 20:03:53 +0530

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,13 @@ import { HoverDirective } from './CustomDirective/hover.directive';
 import { BetterhighlightDirective } from './CustomDirective/betterhighlight.directive';
 import { ClassDirective } from './CustomDirective/class.directive';
 import { StyleDirective } from './CustomDirective/style.directive';
-import { NgIf } from '@angular/common';
+import { NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from '@angular/common';
+import { IfDirective } from './CustomDirective/if.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, setBackgroungDirective, HighlightDirective, HoverDirective, BetterhighlightDirective, ClassDirective, StyleDirective, NgIf],
+  imports: [RouterOutlet, setBackgroungDirective, HighlightDirective, HoverDirective, BetterhighlightDirective, ClassDirective, StyleDirective, NgIf, IfDirective, NgSwitch, NgSwitchCase, NgSwitchDefault],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -21,6 +22,8 @@ export class AppComponent {
   active: boolean = false;
 
   display: boolean = false;
+
+  occupation: string = 'doctor';
 
   DisplayNotice(){
     this.display = true;
